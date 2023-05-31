da72123349bbedf4230dc88cc48e880fc52d87be
Christoforos Psychas <chris.42453@hotmail.com>
2023-05-31 20:25:58 +0300

    Create injectable service and add fetch method

---
+++ myTodoApp/src/app/service/todo-service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoServiceService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users/1/todos'; //URL for the API endpoint that returns todo items
+
+  constructor(private http: HttpClient) { } /*inject an instance of the HttpClient module as a 
+                                              dependency into the TodoService, to allow communication with the 
+                                              HTTP servers and the creation of HTTP requests*/
+
+  getTodoItems(): Observable<any[]> {          /* we use http to send a req to the API, get the todo
+                                               , and return it as an Observable object    */ 
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
